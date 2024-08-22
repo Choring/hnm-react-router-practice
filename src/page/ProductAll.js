cfdcc0218af47e2ac5f92b2196a0125e564e4de7
조인철 <skyhonor201@gmail.com>
2024-08-23 00:53:39 +0900

    api 주소 변경

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 export const ProductAll = () => {
     const [productList, setProductList] = useState([]);
     const getProducts = async () => {
-        let url = `http://localhost:4000/products`;
+        let url = ` https://my-json-server.typicode.com/Choring/hnm-react-router-practice/products`;
         let response = await fetch(url);
         let data = await response.json();
         setProductList(data);
