18d1430ccb1fc9bebf2a6e4754795c04185af6f8
Choring <skyhonor201@gmail.com>
2024-08-24 01:43:44 +0900

    토글버튼 추가

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,14 +1,15 @@
 import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export const Navbar = ({authenticate,setAuthenticate}) => {
     const menuList= ['여성','Divided','남성','신생아/유아','아동','H&M Home','지속가능성']
     const navigate = useNavigate();
     const login = authenticate;
-    
+    const [mobileButton, setMobileButton ] = useState(false);
+
     const search = (event) => {
         if(event.key === "Enter"){
             let keyword = event.target.value;
@@ -17,6 +18,10 @@ export const Navbar = ({authenticate,setAuthenticate}) => {
     }
     const isLogin = () => {
         (login === true) ? setAuthenticate(false) : navigate(`/login`);
+    }
+    const toggleBtn = () => {
+        mobileButton == true ? setMobileButton(false) : setMobileButton(true);
+        console.log(mobileButton);
     }
   return (
     <div>
@@ -30,12 +35,26 @@ export const Navbar = ({authenticate,setAuthenticate}) => {
             <img src='https://www.hm.com/entrance/assets/bundle/img/HM-Share-Image.jpg' width={100} alt='logo'></img>
         </Link>
         <div>
-            <div>
-                <ul className='menu-list'>
-                    {menuList.map((menu,index) => (
-                        <li key={index}>{menu}</li>
-                    ))}
-                </ul>
+            <div className="menu-container">
+                <div className='pc-menu-box'>
+                    <ul className='menu-list'>
+                        {menuList.map((menu,index) => (
+                            <li key={index}>{menu}</li>
+                        ))}
+                    </ul>
+                </div>
+                <button className='toggle-button' onClick={toggleBtn}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <div className='m-menu-box'>
+                    <div className={`${mobileButton == false ? "d-none" : "d-block"}`}>
+                        <ul className="m-menu-list">
+                            {menuList.map((menu,index) => (
+                                <li key={index}>{menu}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className='input-section'>
                 <div className='input-box'>
